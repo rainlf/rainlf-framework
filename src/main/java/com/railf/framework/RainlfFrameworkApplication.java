49a0e874164d48d785a210494ade3649e30b9de5
yhee <yhee@trip.com>
2021-05-19 14:33:48 +0800

    init project

---
+++ src/main/java/com/railf/framework/RainlfFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.railf.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainlfFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainlfFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/railf/framework/RainlfFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railf.framework;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainlfFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
